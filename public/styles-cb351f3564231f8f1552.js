5d3a8d63565b3c83822388a09b7a16bd180d37b1
Jose <hola@jotami.me>
2019-07-07 22:20:19 +0200

    public folder

---
+++ public/styles-cb351f3564231f8f1552.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{195:function(n,o,c){},196:function(n,o,c){},197:function(n,o,c){},199:function(n,o,c){},208:function(n,o,c){}}]);
+//# sourceMappingURL=styles-cb351f3564231f8f1552.js.map
